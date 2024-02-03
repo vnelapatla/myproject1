104c2028b3326a07576437728de6fabeb2fc9f9f
VENKATESH <VENKATESH@DESKTOP-MCAAOES>
2024-02-03 16:26:10 +0530

    initial

---
+++ library-manage-sys-2-dec/src/test/java/com/example/library/project/librarymanagesys2dec/LibraryManageSys2DecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library.project.librarymanagesys2dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManageSys2DecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
